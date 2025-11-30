d06606dc731735acfe821a1ef6f6daee249cc7d1
Chad <chadmaster52@gmail.com>
2025-11-30 16:55:47 -0600

    first implementation of multithreaded sound occlusion ray casting Added debug lines to test in game

--- src/Layers/xrRender/SkeletonRigid.cpp
+++ src/Layers/xrRender/SkeletonRigid.cpp
@@ -5,6 +5,8 @@
 #include "SkeletonCustom.h"
 #include <algorithm>
 #include <execution>
+#include <functional>
+#include <thread>
 
 #include "../../xrCore/profiler.h"
 
@@ -262,6 +264,9 @@ void CKinematics::Bone_Calculate_Parallel(CBoneData *bd, Fmatrix *parent) {
   // Calculate children
   std::for_each(std::execution::par, bd->children.begin(), bd->children.end(),
                 [this, &BONE_INST](CBoneData *C) {
+                  // debug only for testing to verify multithreaded bone
+                  // calculation Msg("Parallel Bone Calc: Thread %d",
+                  // std::hash<std::thread::id>{}(std::this_thread::get_id()));
                   Bone_Calculate_Parallel(C, &BONE_INST.mTransform);
                 });
 }
