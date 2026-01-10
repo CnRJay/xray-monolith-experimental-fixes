9b025112308243fbc9aaaa3e4fa0cbfdba0e71e9
Chad <chadmaster52@gmail.com>
2026-01-10 11:58:36 -0600

    Fix an oopsie with rolling buffer

--- src/Layers/xrRenderDX10/dx10DetailManager_VS.cpp
+++ src/Layers/xrRenderDX10/dx10DetailManager_VS.cpp
@@ -109,7 +109,7 @@ void CDetailManager::hw_Render()
 	//RCache.set_c			(&*hwc_wave,	wave.div(PI_MUL_2));	// wave
 	//RCache.set_c			(&*hwc_wind,	dir1);																					// wind-dir
 	//hw_Render_dump			(&*hwc_array,	1, 0, c_hdr );
-	hw_Render_dump(consts, wave.div(PI_MUL_2), dir1, prev_wave.div(PI_MUL_2), prev_dir1, 1, 0);
+	hw_Render_dump_instanced(consts, wave.div(PI_MUL_2), dir1, prev_wave.div(PI_MUL_2), prev_dir1, 1, 0);
 
 	// Wave1
 	//wave.set				(1.f/3.f,		1.f/7.f,	1.f/5.f,	Device.fTimeGlobal*swing_current.speed);
@@ -118,14 +118,14 @@ void CDetailManager::hw_Render()
 	//RCache.set_c			(&*hwc_wave,	wave.div(PI_MUL_2));	// wave
 	//RCache.set_c			(&*hwc_wind,	dir2);																					// wind-dir
 	//hw_Render_dump			(&*hwc_array,	2, 0, c_hdr );
-	hw_Render_dump(consts, wave.div(PI_MUL_2), dir2, prev_wave.div(PI_MUL_2), prev_dir2, 2, 0);
+	hw_Render_dump_instanced(consts, wave.div(PI_MUL_2), dir2, prev_wave.div(PI_MUL_2), prev_dir2, 2, 0);
 
 	// Still
 	consts.set(scale, scale, scale, 1.f);
 	//RCache.set_c			(&*hwc_s_consts,scale,		scale,		scale,				1.f);
 	//RCache.set_c			(&*hwc_s_xform,	Device.mFullTransform);
 	//hw_Render_dump			(&*hwc_s_array,	0, 1, c_hdr );
-	hw_Render_dump(consts, wave.div(PI_MUL_2), dir2, prev_wave.div(PI_MUL_2), prev_dir2, 0, 1);
+	hw_Render_dump_instanced(consts, wave.div(PI_MUL_2), dir2, prev_wave.div(PI_MUL_2), prev_dir2, 0, 1);
 
 	if (prev_frame != Device.dwFrame) 
 	{
