a45c25e598ecd4615c0edfc0de223b99d854e21c
jebusmaster <noreply@gscw.life>
2026-06-07 16:26:32 +0200

    formatting

--- src/Layers/xrRenderPC_R4/r4_rendertarget_draw_volume.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_draw_volume.cpp
@@ -5,24 +5,24 @@
 
 void CRenderTarget::draw_volume(light* L)
 {
-    switch (L->flags.type)
-    {
-    case IRender_Light::REFLECTED:
-    case IRender_Light::POINT:
-        RCache.set_Geometry(g_accum_point);
+	switch (L->flags.type)
+	{
+	case IRender_Light::REFLECTED:
+	case IRender_Light::POINT:
+		RCache.set_Geometry(g_accum_point);
 		RCache.Render(D3DPT_TRIANGLELIST, 0, 0,DU_SPHERE_NUMVERTEX, 0,DU_SPHERE_NUMFACES);
-        break;
-    case IRender_Light::SPOT:
-        RCache.set_Geometry(g_accum_spot);
+		break;
+	case IRender_Light::SPOT:
+		RCache.set_Geometry(g_accum_spot);
 		RCache.Render(D3DPT_TRIANGLELIST, 0, 0,DU_CONE_NUMVERTEX, 0,DU_CONE_NUMFACES);
-        break;
-    case IRender_Light::OMNIPART:
-        RCache.set_Geometry(g_accum_omnipart);
+		break;
+	case IRender_Light::OMNIPART:
+		RCache.set_Geometry(g_accum_omnipart);
 		RCache.Render(D3DPT_TRIANGLELIST, 0, 0,DU_SPHERE_PART_NUMVERTEX, 0,DU_SPHERE_PART_NUMFACES);
-        break;
-    default:
-        break;
-    }
+		break;
+	default:
+		break;
+	}
 }
 
 void CRenderTarget::draw_occq_volume(light* L)
