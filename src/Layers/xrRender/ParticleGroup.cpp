14be9f8e946169d3f26183f2836f7f6ce5a46d9f
Chad <chadmaster52@gmail.com>
2026-06-12 13:00:10 -0500

    particle group fallback when less than 4 children to avoid tbb overhead

--- src/Layers/xrRender/ParticleGroup.cpp
+++ src/Layers/xrRender/ParticleGroup.cpp
@@ -519,13 +519,36 @@ void CParticleGroup::SItem::OnFrame(u32 u_dt, const CPGDef::SEffect &def,
       }
     };
 
-    UpdateBody body(_children_related, u_dt, def);
-    tbb::parallel_reduce(tbb::blocked_range<int>(0, _children_related.size()),
-                         body);
+    if (_children_related.size() < 4u) {
+      static thread_local CollisionContext ctx;
+      ctx.r_spatial.clear();
+      ctx.r_temp.r_clear();
+      for (size_t i = 0; i < _children_related.size(); ++i) {
+        CParticleEffect *E = static_cast<CParticleEffect *>(_children_related[i]);
+        if (E) {
+          E->SetCollisionContext(&ctx);
+          E->OnFrame(u_dt);
+          E->SetCollisionContext(nullptr);
+          if (E->IsPlaying()) {
+            bPlaying = true;
+            if (E->vis.box.is_valid())
+              box.merge(E->vis.box);
+          } else {
+            if (def.m_Flags.is(CPGDef::SEffect::flOnPlayChildRewind)) {
+              E->Play();
+            }
+          }
+        }
+      }
+    } else {
+      UpdateBody body(_children_related, u_dt, def);
+      tbb::parallel_reduce(tbb::blocked_range<int>(0, _children_related.size()),
+                           body);
 
-    if (body.bPlaying)
-      bPlaying = true;
-    box.merge(body.box);
+      if (body.bPlaying)
+        bPlaying = true;
+      box.merge(body.box);
+    }
   }
 
   // Parallel update for _children_free (Pass 1: Update)
@@ -571,13 +594,32 @@ void CParticleGroup::SItem::OnFrame(u32 u_dt, const CPGDef::SEffect &def,
       }
     };
 
-    UpdateFreeBody body(_children_free, u_dt);
-    tbb::parallel_reduce(tbb::blocked_range<int>(0, _children_free.size()),
-                         body);
+    if (_children_free.size() < 4u) {
+      static thread_local CollisionContext ctx;
+      ctx.r_spatial.clear();
+      ctx.r_temp.r_clear();
+      for (size_t i = 0; i < _children_free.size(); ++i) {
+        CParticleEffect *E = static_cast<CParticleEffect *>(_children_free[i]);
+        if (E) {
+          E->SetCollisionContext(&ctx);
+          E->OnFrame(u_dt);
+          E->SetCollisionContext(nullptr);
+          if (E->IsPlaying()) {
+            bPlaying = true;
+            if (E->vis.box.is_valid())
+              box.merge(E->vis.box);
+          }
+        }
+      }
+    } else {
+      UpdateFreeBody body(_children_free, u_dt);
+      tbb::parallel_reduce(tbb::blocked_range<int>(0, _children_free.size()),
+                           body);
 
-    if (body.bPlaying)
-      bPlaying = true;
-    box.merge(body.box);
+      if (body.bPlaying)
+        bPlaying = true;
+      box.merge(body.box);
+    }
 
     // Pass 2: Mark finished children for deferred deletion
     for (VisualVecIt it = _children_free.begin(); it != _children_free.end(); it++) {
